e555554ba3e546ff3530b69a36c6243e0779f57b
Brett R. Gebbie <brg078@gmail.com>
2023-01-21 16:20:20 -0600

    mui import

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Brett R. Gebbie
         </p>
       </header>
+      <body>
+        info info
+      </body>
     </div>
   );
 }
